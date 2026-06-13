97c0294d074e6fe705ec8e3a485524bda1debb70
Sushant Raj <sushantraj62060@gmail.com>
2026-06-13 23:48:18 +0530

    game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -2,6 +2,9 @@ from utils import roll_dice
 
 
 def play_round(user_choice):
+    if not 1 <= user_choice <= 6:
+        raise ValueError("Dice value must be between 1 and 6")
+        
     comp_choice = roll_dice()
     return user_choice, comp_choice
 
